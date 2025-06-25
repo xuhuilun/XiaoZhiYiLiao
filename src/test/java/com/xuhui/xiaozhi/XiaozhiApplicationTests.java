fdd32a5f1e373f82076f8d258c1a8f5820bb4d79
LunXuHui <13346639839@163.com>
2025-06-25 12:54:08 +0800

    Initial commit

---
+++ src/test/java/com/xuhui/xiaozhi/XiaozhiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xuhui.xiaozhi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XiaozhiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
